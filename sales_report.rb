b0804e12f1f01ca3e35588b009ee489d4a55dec8
Aaron Kuehler <aaron.kuehler@gmail.com>
2024-03-09 10:29:09 -0500

    Step 2. Rename variable report result

--- sales_report.rb
+++ sales_report.rb
@@ -9,21 +9,13 @@ class SalesReport
       { rep: "Sam", region: "North East", revenue: "10_000" },
     ]
 
-    # 2. This is the main computation bit of the current report, and
-    # it seems like we'll need to add more computation logic to suppor
-    # the new requirement. While this bit is aesthetically atrocious,
-    # it's functional. Its readability is poor, but so far as the
-    # system is concerned it computes the correct values for the
-    # report and it is not immediately clear this NEEDS to change to
-    # accept the new requirement. This section is NOT a primary
-    # concern for me at this point.
-    regional_revenue = Hash.new(0)
+    # [mf-refactoring 137] Rename variable.
+    result = Hash.new(0)
     for rep in sales
-      regional_revenue[rep.fetch(:region)] += rep.fetch(:revenue).to_i
+      result[rep.fetch(:region)] += rep.fetch(:revenue).to_i
     end
 
-    # 1. The report currently consists of ONLY the regional revenue. This is the most obvious site which needs to change
-    regional_revenue
+    result
   end
 end
 
@@ -36,12 +28,12 @@ class SalesReportTest < Minitest::Test
   end
 end
 
-# >> Run options: --seed 12117
+# >> Run options: --seed 45460
 # >>
 # >> # Running:
 # >>
 # >> .
 # >>
-# >> Finished in 0.000326s, 3067.4849 runs/s, 3067.4849 assertions/s.
+# >> Finished in 0.000321s, 3115.2648 runs/s, 3115.2648 assertions/s.
 # >>
 # >> 1 runs, 1 assertions, 0 failures, 0 errors, 0 skips
